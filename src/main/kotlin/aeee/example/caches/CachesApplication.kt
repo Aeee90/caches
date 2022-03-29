0b4a156a445f0f3c0e33eeb073e430f4e942fe97
seoyeon <sylee@tanagement.net>
2022-03-29 21:34:17 +0900

    init

---
+++ src/main/kotlin/aeee/example/caches/CachesApplication.kt
@@ -0,0 +1,11 @@
+package aeee.example.caches
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CachesApplication
+
+fun main(args: Array<String>) {
+    runApplication<CachesApplication>(*args)
+}
---
+++ src/test/kotlin/aeee/example/caches/CachesApplicationTests.kt
@@ -0,0 +1,13 @@
+package aeee.example.caches
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CachesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
